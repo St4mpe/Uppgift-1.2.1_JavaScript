263653f1f5d03ec8afa6167ffc23347a7364b3b6
St4mpe <hampusekmansundin@gmail.com>
2025-09-30 14:44:02 +0200

    Works

---
+++ script.js
@@ -0,0 +1,21 @@
+const txt_box=document.getElementById("txt_box");
+const bg_theme=document.getElementById("bg_theme");
+
+document.addEventListener("DOMContentLoaded", function(){
+    txt_box.addEventListener('click', function(){
+        if (txt_box.classList == "text_for_dark")
+        {
+            txt_box.classList.remove("text_for_dark");
+            txt_box.classList.add("text_for_light");
+            bg_theme.classList.add("clr_for_bg_light");
+            bg_theme.classList.remove("clr_for_bg_dark");
+        }
+        else if(txt_box.classList == "text_for_light")
+        {
+            txt_box.classList.add("text_for_dark");
+            txt_box.classList.remove("text_for_light");
+            bg_theme.classList.remove("clr_for_bg_light");
+            bg_theme.classList.add("clr_for_bg_dark");
+        }
+    })
+})
